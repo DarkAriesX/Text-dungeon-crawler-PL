f686b38b745ef526bb88dc2ebf1c5b10a8480c10
DarkAriesX <skrzys2103@gmail.com>
2020-05-14 09:32:14 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "POZIOMY/pokoj-1.cpp"
+#include "POZIOMY/pokoj-2.cpp"
+#include "POZIOMY/pokoj-3.cpp"
+
+#include "KLASY PRZECIWNIKÓW/Pajak.cpp"
+#include "KLASY PRZECIWNIKÓW/Szczur.cpp"
+
+#include "FUNKCJE/MENU.h"
+
+int main()
+{
+    funkcje::menu();
+    
+    return 0;
+}
