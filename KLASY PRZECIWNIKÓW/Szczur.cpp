6aaeea1b64b41ca24df03d6a4ef6e9a729dd3cf0
DarkAriesX <skrzys2103@gmail.com>
2020-05-14 09:30:34 +0200

    Katalog z przeciwnikami

---
+++ KLASY PRZECIWNIKÓW/Szczur.cpp
@@ -0,0 +1,24 @@
+namespace szczur
+{
+    class lvl1
+    {
+        public:
+            short int HP = 10;
+            short int ATK = 5;
+            short int DEF  = 1;
+    };
+    class lvl2
+    {
+        public:
+            short int HP = 13;
+            short int ATK = 6;
+            short int DEF = 2;
+    };
+    class lvl3
+    {
+        public:
+            short int HP = 15;
+            short int ATK = 7;
+            short int DEF = 3;
+    };
+}
